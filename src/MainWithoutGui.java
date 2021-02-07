d0dbf0adf4ecd611cc45b53e8f12c17b96e730ce
ABSabellon <aileen.sabellon@gmail.com>
2021-02-07 19:36:25 +0800

    GUIs

--- src/MainWithoutGui.java
+++ src/MainWithoutGui.java
@@ -1,13 +1,14 @@
 import ui.GUI;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class MainWithoutGui {
 
     public static void main(String[] args) throws IOException {
-        ExprLexer lexer = new ExprLexer(new InputStreamReader(System.in));
+       /* ExprLexer lexer = new ExprLexer(new InputStreamReader(System.in));
         ExprParser parser = new ExprParser(lexer);
         parser.expr();
-        System.out.println("ACCEPTED");  // prints if no exception
+        System.out.println("ACCEPTED");  // prints if no exception*/
     }
 }
